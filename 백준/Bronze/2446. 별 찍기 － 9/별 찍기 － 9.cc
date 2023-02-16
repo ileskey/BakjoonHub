fe2a50ea30a870a2d00e63c9bc4881c593d1f39a
ileskey <68210936+ileskey@users.noreply.github.com>
2023-02-16 15:18:08 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a,b,c;
+    cin >> a;
+    for (b = 1; b <= a; b++) {
+        for (c = 1; c < b; c++) cout << " ";
+        for (c = a - b; c > 0; c--) cout << "**";
+        cout << "*\n";
+    }
+    for (b = 1; b < a; b++) {
+        for (c = a - b; c > 1; c--) cout << " ";
+        for (c = 0; c < b; c++) cout << "**";
+        cout << "*\n";
+    }
+}
