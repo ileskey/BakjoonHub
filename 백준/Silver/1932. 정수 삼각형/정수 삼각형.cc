add3ae5bd41c049faea9e09459d7149190b1d464
ileskey <68210936+ileskey@users.noreply.github.com>
2023-03-14 14:50:22 +0900

    [Silver I] Title: 정수 삼각형, Time: 40 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -7,7 +7,7 @@ int main() {
 	for (a = 0; a < N; a++) arr[a] = new int[N] {};
 	for (a = 0; a < N; a++)for (b = 0; b <= a; b++) cin >> arr[a][b];
 	for (a = 1; a < N; a++) {
-		for (b = 0; b < N; b++) {
+		for (b = 0; b <= a; b++) {
 			l = 0, r = 0;
 			if (b > 0)l = arr[a - 1][b - 1];
 			if (b < N)r = arr[a - 1][b];
