301164d5434ae4b213f9f78f9c588b8d33410e79
ileskey <68210936+ileskey@users.noreply.github.com>
2023-04-10 01:03:57 +0900

    [Bronze I] Title: 2진수 8진수, Time: 36 ms, Memory: 3324 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.cc
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.cc
@@ -1,12 +1,13 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
-string s;
 char arr[1000001];
+char s[333334];
 int a, b;
 void fnc(int c) {
+	int d = c ? 1 : 0;
 	for(a = 0;a < b; a++)
-		s += ((arr[a * 3+c] - '0') * 4 + (arr[a * 3+c + 1] - '0') * 2 + (arr[a * 3+c + 2]));
+		s[a+d] = ((arr[a * 3 + c] - '0') * 4 + (arr[a * 3 + c + 1] - '0') * 2 + (arr[a * 3 + c + 2]));
 }
 int main() {
 	cin >> arr;
@@ -18,18 +19,18 @@ int main() {
 			fnc(0);
 		}
 		else if (c == 1) {
-			s += arr[0];
+			s[0] = arr[0];
 			fnc(1);
 		}
 		else {
-			s += (arr[0] - '0') * 2 + arr[1];
+			s[0] = (arr[0] - '0') * 2 + arr[1];
 			fnc(2);
 		}
 	}
 	else if (b == 2) {
-		s += (arr[0] - '0') * 2 + arr[1];
+		s[0] = (arr[0] - '0') * 2 + arr[1];
 	}
 	else
-		s += arr[0];
+		s[0] = arr[0];
 	cout << s;
 }
