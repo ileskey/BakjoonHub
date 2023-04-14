6295efd0b7936a8c61e44e66fe50ed47e8ccfeeb
ileskey <68210936+ileskey@users.noreply.github.com>
2023-04-14 13:18:36 +0900

    [Bronze II] Title: 분산처리, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.cc
+++ 백준/Bronze/1009. 분산처리/분산처리.cc
@@ -2,12 +2,21 @@
 using namespace std;
 int main() {
 	int N, a, b, c;
+	int a2[]{ 6,2,4,8 }, a3[]{ 1,3,9,7 }, a4[]{ 6,4 }, a5[]{ 5 }, a6[]{ 6 }, a7[]{ 1,7,9,3 }, a8[]{ 6,8,4,2 }, a9[]{ 1,9 };
 	cin >> N;
 	while (N--) {
 		cin >> a >> b;
-		c = 1;
-		while (b--) (c *= a) %= 10;
-		if (c == 0)c = 10;
+		a %= 10;
+		if (a == 1) c = 1;
+		else if (a == 2) c = a2[b % 4];
+		else if (a == 3) c = a3[b % 4];
+		else if (a == 4) c = a4[b % 2];
+		else if (a == 5) c = 5;
+		else if (a == 6) c = 6;
+		else if (a == 7) c = a7[b % 4];
+		else if (a == 8) c = a8[b % 4];
+		else if (a == 9) c = a9[b % 2];
+		else c = 10;
 		cout << c << "\n";
 	}
-}
+}	
