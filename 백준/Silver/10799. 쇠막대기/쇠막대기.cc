9b988f54c91c34e0c8c1a0bd735b19cf9fa36550
ileskey <68210936+ileskey@users.noreply.github.com>
2023-11-21 15:43:16 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stack>
+#include<string.h>
+int main() {
+	char arr[100000];
+	scanf("%s", arr);
+	int idx = 0, sum = 0;
+	int len = strlen(arr);
+	for (int i = 0; i < len; i++) {
+		if (arr[i] == '(') idx++;
+		else {
+			if (arr[i - 1] == '(') {
+				idx--;
+				sum += idx;
+			}
+			else {
+				idx--;
+				sum++;
+			}
+		}
+	}
+	printf("%d\n", sum);
+}
