7c518ce83489f71a7225bb02c0903f768c4a54f9
Baek Jun-u <68210936+ileskey@users.noreply.github.com>
2024-10-23 14:28:52 +0900

    [Silver I] Title: 언더프라임, Time: 804 ms, Memory: 2424 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.cc
+++ 백준/Silver/1124. 언더프라임/언더프라임.cc
@@ -2,31 +2,37 @@
 #include<vector>
 using namespace std;
 int main() {
-	long long int A, B, i, j, n = 0, k, temp, ite;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	long long int A, B, i, j, n = 0, k, temp, ite, vsize;
 	vector<int> v;
 	cin >> A >> B;
-	long long int arr[100001] = { 0, };
-	for (i = 2; i <= B; i++) {
+	long long int arr[50001] = { 0, };
+	for (i = 2; i < 50001; i++) {
 		if (arr[i] == 0) {
 			v.push_back(i);
-			for (j = i * i; j < 100001; j += i) {
+			for (j = i * i; j < 50001; j += i) {
 				arr[j] = 1;
 			}
 		}
 	}
+	vsize = v.size();
 	for (i = A; i <= B; i++) {
 		k = 0;
 		ite = 0;
 		temp = i;
 		while (temp != 1) {
+			if (ite == vsize && temp > 50000) {
+				k++;
+				break;
+			}
 			if (temp % v[ite] == 0) {
 				k++;
 				temp /= v[ite];
 			}
 			else ite++;
 		}
-		temp = v.size();
-		for (ite = 0; ite < temp; ite++) {
+		for (ite = 0; ite < vsize; ite++) {
 			if (k == v[ite]) { n++; break; }
 		}
 	}
