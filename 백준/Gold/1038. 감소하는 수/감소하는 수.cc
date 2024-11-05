d5197b4b51d859d5ceee16d0b76b0bfd6d6db76c
Baek Jun-u <68210936+ileskey@users.noreply.github.com>
2024-11-05 11:49:35 +0900

    [Gold V] Title: 감소하는 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.cc
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int K, N, cnt;
+int arr_arr[11];
+int arr_did[11];
+void func(int n, int k) {
+	if (n == 0) {
+		cnt++;
+		if (N == cnt) {
+			long long ans = 0;
+			for (int i = K; i > 0; i--) {
+				ans *= 10;
+				ans += arr_arr[i];
+			}
+			cout << ans << "\n";
+		}
+		return;
+	}
+	for (int j = n - 1; j < k; j++) {
+		if (arr_did[j])continue;
+		arr_did[j] = 1;
+		arr_arr[n] = j;
+		func(n - 1, j);
+		arr_did[j] = 0;
+	}
+}
+int main() {
+	int n = 1;
+	K = 1;
+	cin >> N;
+	if (!N) {
+		cout << 0;
+		return 0;
+	}
+	N++;
+	while (K <= 10) {
+		n *= (11 - K);
+		n /= K;
+		if (N - n > 0) N -= n;
+		else break;
+		K++;
+	}
+	if (N > 0 && K > 10) {
+		cout << -1;
+		return 0;
+	}
+	func(K, 10);
+	return 0;
+}
