112027c47851f0b782524d9f4dbb6ab7b1132e26
Baek Jun-u <68210936+ileskey@users.noreply.github.com>
2025-07-25 11:13:55 +0900

    [Silver IV] Title: 점프왕 최준민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11564. 점프왕 최준민/점프왕 최준민.cc
+++ 백준/Silver/11564. 점프왕 최준민/점프왕 최준민.cc
@@ -3,19 +3,15 @@ using namespace std;
 int main() {
 	long long X, Y, Z, sum = 0;
 	cin >> X >> Y >> Z;
+	sum += Z / X;
+	sum -= Y / X;
 	if (Y > 0) {
-		sum += Z / X;
-		sum -= Y / X;
 		if (!(Y % X))sum++;
 	}
 	else if (Z < 0) {
-		sum -= Y / X;
-		sum += Z / X;
 		if (!(Z % X))sum++;
 	}
 	else {
-		sum += Z / X;
-		sum -= Y / X;
 		sum++;
 	}
 	cout << sum;
